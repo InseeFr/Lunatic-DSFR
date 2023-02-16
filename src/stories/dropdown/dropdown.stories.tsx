45a42f0531890fce5b902e6508253675de436dff
James ABIB <61329786+jabibamman@users.noreply.github.com>
2023-02-16 13:30:07 +0000

    Adjustments for dropdown stories (default, error, success)

--- src/stories/dropdown/dropdown.stories.tsx
+++ src/stories/dropdown/dropdown.stories.tsx
@@ -1,21 +1,62 @@
 // import React from 'react';
-import Orchestrator from "../utils/Orchestrator";
 import source from "./source.json";
-import data from "./data.json";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 import defaultArgTypes from "../utils/default-arg-types";
-import * as custom from "../../";
+import { Dropdown } from "../../";
 
+const options = source.components[0].options.map((option: any) => ({
+    value: option.value,
+    label: {
+        props: {
+            expression: option.label.value,
+        },
+    },
+}));
 const stories = {
     title: "Components/Dropdown",
-    component: Orchestrator,
+    component: Dropdown,
     argTypes: defaultArgTypes,
-} as ComponentMeta<typeof Orchestrator>;
+} as ComponentMeta<typeof Dropdown>;
 
 export default stories;
 
-const Template: ComponentStory<typeof Orchestrator> = args => <Orchestrator {...args} custom={custom} />;
+const Template: ComponentStory<typeof Dropdown> = args => (
+    <>
+        <Dropdown
+            {...args}
+            options={options}
+            state="default"
+            stateRelatedMessage={source.components[0].controls.stateRelatedMessage.default}
+        />
+    </>
+);
+  
+const TemplateError: ComponentStory<typeof Dropdown> = args => (
+    <>
+        <Dropdown
+            {...args}
+            options={options}
+            state="error"
+            stateRelatedMessage={source.components[0].controls.stateRelatedMessage.error}
+        />
+    </>
+);
+
+const TemplateSuccess: ComponentStory<typeof Dropdown> = args => (
+    <>
+        <Dropdown
+            {...args}
+            options={options}
+            state="success"
+            stateRelatedMessage={source.components[0].controls.stateRelatedMessage.success}
+        />
+    </>
+);
 
 export const Default = Template.bind({});
+export const Error = TemplateError.bind({});
+export const Success = TemplateSuccess.bind({});
 
-Default.args = { source, data };
+Default.args = {};
+Error.args = {};
+Success.args = {};
