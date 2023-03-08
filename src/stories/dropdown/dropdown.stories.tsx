fa8616e7e513b1c8c43c6fc164cc843ad90a5e84
Matthew Clark <matthew.clark@insee.fr>
2023-03-08 14:53:36 +0100

    use same logic from Input errors in dropdown

--- src/stories/dropdown/dropdown.stories.tsx
+++ src/stories/dropdown/dropdown.stories.tsx
@@ -1,63 +1,21 @@
 // import React from 'react';
 import source from "./source.json";
+import data from "./data.json";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 import defaultArgTypes from "../utils/default-arg-types";
-import { Dropdown } from "../../";
+import Orchestrator from "../utils/Orchestrator";
+import * as custom from "../../";
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const options = source.components[0].options.map((option: any) => ({
-    value: option.value,
-    label: {
-        props: {
-            expression: option.label.value,
-        },
-    },
-}));
 const stories = {
     title: "Components/Dropdown",
-    component: Dropdown,
+    component: Orchestrator,
     argTypes: defaultArgTypes,
-} as ComponentMeta<typeof Dropdown>;
+} as ComponentMeta<typeof Orchestrator>;
 
 export default stories;
 
-const Template: ComponentStory<typeof Dropdown> = args => (
-    <>
-        <Dropdown
-            {...args}
-            options={options}
-            state="default"
-            stateRelatedMessage={source.components[0].controls.stateRelatedMessage.default}
-        />
-    </>
-);
-
-const TemplateError: ComponentStory<typeof Dropdown> = args => (
-    <>
-        <Dropdown
-            {...args}
-            options={options}
-            state="error"
-            stateRelatedMessage={source.components[0].controls.stateRelatedMessage.error}
-        />
-    </>
-);
-
-const TemplateSuccess: ComponentStory<typeof Dropdown> = args => (
-    <>
-        <Dropdown
-            {...args}
-            options={options}
-            state="success"
-            stateRelatedMessage={source.components[0].controls.stateRelatedMessage.success}
-        />
-    </>
-);
+const Template: ComponentStory<typeof Orchestrator> = args => <Orchestrator {...args} custom={custom} />;
 
 export const Default = Template.bind({});
-export const Error = TemplateError.bind({});
-export const Success = TemplateSuccess.bind({});
 
-Default.args = {};
-Error.args = {};
-Success.args = {};
+Default.args = { source, data };
