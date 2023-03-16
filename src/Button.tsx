4c582165feb324b95c47128938c99308fa2aceb5
Matthew Clark <matthew.clark@insee.fr>
2023-03-16 10:35:00 +0100

    Separated button stories by state

--- src/Button.tsx
+++ src/Button.tsx
@@ -5,33 +5,21 @@ import { Button as ButtonDSFR } from "@codegouvfr/react-dsfr/Button";
 
 export function Button({
     children,
-    onClick,
     disabled,
     className,
     priority,
 }: {
     children: string | React.ReactNode;
-    onClick: React.MouseEventHandler<HTMLButtonElement>;
     disabled: boolean;
     // label: string,
     className: string;
     priority?: "primary" | "secondary" | "tertiary" | "tertiary no outline" | undefined;
 }) {
-    const handleClick = useCallback(
-        function (e: React.MouseEvent<HTMLButtonElement>) {
-            e.stopPropagation();
-            e.preventDefault();
-            onClick(e);
-        },
-        [onClick],
-    );
-
     if (isElement(children))
         return (
             <ButtonDSFR
                 disabled={disabled}
                 className={classnames("button-lunatic-dsfr", className, { disabled })}
-                onClick={handleClick}
                 priority={priority}
             >
                 {children}
@@ -42,7 +30,6 @@ export function Button({
         <ButtonDSFR
             disabled={disabled}
             className={classnames("button-lunatic-dsfr", className, { disabled })}
-            onClick={handleClick}
             priority={priority}
         >
             {children}
