da52ece7dcf24d0dc0cf78e176c237774de25335
Matthew Clark <matthew.clark@insee.fr>
2023-04-05 11:47:13 +0200

    remove unnecessary console.log)

--- src/stories/utils/Orchestrator.tsx
+++ src/stories/utils/Orchestrator.tsx
@@ -157,7 +157,6 @@ const Orchestrator: FC<OrchestratorProps> = ({
                         response?: string;
                         storeName?: string;
                     }) {
-                        console.log(currentErrors);
                         const { id, componentType, storeName, response, ...other } = component;
                         const Component = lunatic[componentType];
                         const storeInfo = storeName ? getStoreInfo(storeName) : {};
