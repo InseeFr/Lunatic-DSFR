9e0de9867d715548fb21e25bae681317ee1e3691
Julia Dirand <julia.dirand@insee.fr>
2023-05-11 15:55:25 +0200

    Add sequences and subsequences

---
+++ src/Sequence.tsx
@@ -0,0 +1,25 @@
+import { fr } from "@codegouvfr/react-dsfr";
+import { useColors } from "@codegouvfr/react-dsfr/useColors";
+import { Declarations } from "@inseefr/lunatic";
+import { DeclarationType } from "./utils/type/type-source";
+type SequenceType = {
+    label: string;
+    declarations: Array<DeclarationType>;
+};
+
+const afterPostion = "AFTER_QUESTION_TEXT";
+
+export function Sequence({ label, declarations }: SequenceType) {
+    const theme = useColors();
+    return (
+        <div
+            className={fr.cx("fr-grid-row", "fr-p-4w")}
+            style={{ backgroundColor: theme.decisions.background.alt.grey.default }}
+        >
+            <h2 className={fr.cx("fr-h3", "fr-col-12")}>{label}</h2>
+            <Declarations type={afterPostion} declarations={declarations} />
+        </div>
+    );
+}
+
+export default Sequence;
---
+++ src/Subsequence.tsx
@@ -0,0 +1,18 @@
+import { fr } from "@codegouvfr/react-dsfr";
+import { Declarations } from "@inseefr/lunatic";
+import { DeclarationType } from "./utils/type/type-source";
+type SubsequenceType = {
+    label: string;
+    declarations: Array<DeclarationType>;
+};
+const afterPostion = "AFTER_QUESTION_TEXT";
+export function Subsequence({ label, declarations }: SubsequenceType) {
+    return (
+        <div>
+            <h2 className={fr.cx("fr-h4", "fr-col-12")}>{label}</h2>
+            <Declarations type={afterPostion} declarations={declarations} />
+        </div>
+    );
+}
+
+export default Subsequence;
