8c839039d091028e4b4818c33aea078733a2e53a
Matthew Clark <matthew.clark@insee.fr>
2023-09-19 17:20:40 +0200

    add story to test function

--- src/QuestionExplication.tsx
+++ src/QuestionExplication.tsx
@@ -1,6 +1,7 @@
-import { Accordion as AccordionDSFR } from "@codegouvfr/react-dsfr/Accordion";
 import { useColors } from "@codegouvfr/react-dsfr/useColors";
 import { themeStringToVariable } from "./utils/themeStringToVariable";
+import { useCallback, useEffect, useState } from "react";
+import { fr } from "@codegouvfr/react-dsfr";
 
 type QuestionExplicationType = {
     label: string;
@@ -10,6 +11,18 @@ type QuestionExplicationType = {
 
 export function QuestionExplication({ label, description, bgColor }: QuestionExplicationType) {
     const theme = useColors();
+    const [expanded, setExpanded] = useState<boolean>(false);
+
+    useEffect(() => {
+        if (expanded === true) {
+            setExpanded(false);
+        }
+    }, [label]);
+
+    const handleExpandedChange = useCallback(() => {
+        setExpanded(!expanded);
+    }, [label, expanded]);
+
     const backgroundColor = themeStringToVariable(
         theme,
         bgColor,
@@ -24,9 +37,21 @@ export function QuestionExplication({ label, description, bgColor }: QuestionExp
             className="fr-py-2w"
         >
             <div className={"fr-container fr-grid-row fr-grid-row--center fr-grid-row--middle fr-px-1w"}>
-                <AccordionDSFR label={label} className={"fr-col-lg-6 fr-col-md-9 fr-col-12"}>
-                    {description}
-                </AccordionDSFR>
+                <section className={fr.cx("fr-accordion", "fr-col-lg-6", "fr-col-md-9", "fr-col-12")}>
+                    <h3 className={fr.cx("fr-accordion__title")}>
+                        <button
+                            className={fr.cx("fr-accordion__btn")}
+                            aria-expanded={expanded}
+                            aria-controls="accordion-106"
+                            onClick={handleExpandedChange}
+                        >
+                            {label}
+                        </button>
+                    </h3>
+                    <div className={fr.cx("fr-collapse")} id="accordion-106">
+                        {description}
+                    </div>
+                </section>
             </div>
         </div>
     );
