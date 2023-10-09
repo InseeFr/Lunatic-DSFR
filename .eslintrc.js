f4cf37f6dc82d12ddb65ebdec314b6f470e757d3
Julia Dirand <julia.dirand@insee.fr>
2023-10-09 15:05:41 +0200

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
   "plugins": ["@typescript-eslint"],
   "extends": ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier", "plugin:storybook/recommended"],
   "rules": {
+     "@typescript-eslint/no-unnecessary-type-constraint": "error",
     "no-extra-boolean-cast": "off",
     "@typescript-eslint/explicit-module-boundary-types": "off"
   }
