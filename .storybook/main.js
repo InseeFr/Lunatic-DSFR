2df8020485a471fd72f75d9e44495a39796c633b
Julia Dirand <julia.dirand@insee.fr>
2023-10-09 13:27:14 +0200

    feat:suggester stories

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,7 +4,7 @@ const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
 
 module.exports = {
     stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-
+    staticDirs: ["../public"],
     features: {
         storyStoreV7: false,
     },
