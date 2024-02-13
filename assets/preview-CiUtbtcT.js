18aa57118cfe80e30621a5180b23b64e0f83208a
ddecrulle <ddecrulle@users.noreply.github.com>
2024-02-13 15:56:58 +0000

    deploy: a775b56a205056413e181448f4e50659f71f9449

--- assets/preview-CiUtbtcT.js
+++
@@ -1,7 +0,0 @@
-import{_ as e}from"./iframe-C2A3HTkk.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-abzgi03o.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9,10,11]),import.meta.url);return new r}}};export{a as parameters};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-abzgi03o.js","./chunk-HLWAVYOI-DR370-qS.js","./iframe-C2A3HTkk.js","./index-CBqU2yxZ.js","./_commonjsHelpers-BosuxZz1.js","./react-18-D8cruF67.js","./index-BtM5VmRH.js","./chunk-ZGA76URP-CA258KPk.js","./extends-BDbCiz7i.js","./index-Bw8VTzHM.js","./index-Cmc67Rxs.js","./index-DrFu-skq.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
