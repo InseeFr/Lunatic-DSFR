d2f18bf362c5ba7404f09693ad611f8da355866a
ddecrulle <ddecrulle@users.noreply.github.com>
2024-03-18 13:27:37 +0000

    deploy: 1b62cf38bf26f8fa2b62369fe2958db59623333b

---
+++ storybook/assets/checkboxBoolean.orchestrated.stories-w4zH4YVP.js
@@ -0,0 +1,13 @@
+import{j as a}from"./jsx-runtime-2xDJh5tt.js";import{O as s}from"./Orchestrator-BulEV6K8.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-Dk74W0Oi.js";import"./index-BtM5VmRH.js";import"./i18n-Cg-UD1N1.js";import"./inheritsLoose-Cay3MKMD.js";const l={cleaning:{},variables:[{variableType:"COLLECTED",values:{COLLECTED:null,EDITED:null,INPUTED:null,FORCED:null,PREVIOUS:null},name:"CHECKBOXBoolean"}],components:[{componentType:"CheckboxBoolean",bindingDependencies:["CHECKBOXBoolean"],response:{name:"CHECKBOXBoolean"},conditionFilter:{type:"VTL",value:"true"},id:"ltfrftp8",page:"1",label:{type:"VTL|MD",value:'"Checkbox Boolean"'},mandatory:!1}],pagination:"question",resizing:{},label:{type:"VTL|MD",value:"Test-Dylan"},lunaticModelVersion:"2.5.0",modele:"TESTDYLAN",enoCoreVersion:"2.7.1",generatingDate:"06-03-2024 12:46:44",missing:!1,id:"lsvuvtbg",maxPage:"1"},b={title:"Components/CheckboxBoolean",component:s,args:{source:l}},e={render:o=>(console.log(o),a(s,{...o})),parameters:{docs:{description:{story:"This component should be used when you want the user to be able to select only one answers."}}}};var n,r,t;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  render: args => {
+    console.log(args);
+    return <Orchestrator {...args} />;
+  },
+  parameters: {
+    docs: {
+      description: {
+        story: "This component should be used when you want the user to be able to select only one answers."
+      }
+    }
+  }
+}`,...(t=(r=e.parameters)==null?void 0:r.docs)==null?void 0:t.source}}};const D=["Default"];export{e as Default,D as __namedExportsOrder,b as default};
