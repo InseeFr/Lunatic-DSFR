326799def51a7ada9ab291d76425963e4169f4df
ddecrulle <ddecrulle@users.noreply.github.com>
2024-03-27 10:35:56 +0000

    deploy: 0aba8c85460dc63eae2c8a7043593eb20247acaa

---
+++ storybook/assets/preview-DVqKhw8I.js
@@ -0,0 +1,7 @@
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-dvwflXPV.js","./chunk-HLWAVYOI-uGto5Cae.js","./iframe-BlvRNHtw.js","./index-CBqU2yxZ.js","./_commonjsHelpers-BosuxZz1.js","./react-18-D8cruF67.js","./index-BtM5VmRH.js","./inheritsLoose-Cay3MKMD.js","./index-C-I6vmrZ.js","./index-CWLWFqC4.js","./index-DrFu-skq.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
+import{_ as e}from"./iframe-BlvRNHtw.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-dvwflXPV.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9,10]),import.meta.url);return new r}}};export{a as parameters};
