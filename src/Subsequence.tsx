160b47f5cc60da9b6c6f511473055e46a6896f41
Dylan Decrulle <81740200+ddecrulle@users.noreply.github.com>
2024-03-29 14:51:41 +0100

    fix build

--- src/Subsequence.tsx
+++ src/Subsequence.tsx
@@ -2,7 +2,6 @@ import type { LunaticSlotComponents } from "@inseefr/lunatic";
 import { Declarations } from "./Declarations";
 
 export const Subsequence: LunaticSlotComponents["Subsequence"] = props => {
-    // @ts-expect-error waiting lunatic type (in 3.0.0-rc.19)
     const { id, label, declarations, description } = props;
     return (
         <div id={id}>
