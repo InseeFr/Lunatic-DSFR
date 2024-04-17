d0c6641de1c59c981ee159620325407aa2e625fe
Dylan Decrulle <81740200+ddecrulle@users.noreply.github.com>
2024-04-17 15:12:56 +0200

    remove pagination stories

--- src/stories/Orchestrator.tsx
+++ src/stories/Orchestrator.tsx
@@ -69,7 +69,6 @@ export const Orchestrator: (props: OrchestratorProps) => JSX.Element = ({
         }
     };
 
-    console.log(errorActive);
     return (
         <Provider>
             <div className="container">
