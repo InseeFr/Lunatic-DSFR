c7dc1e7c67a6084324623917043cf3cc4ca5ecf2
ddecrulle <ddecrulle@users.noreply.github.com>
2024-05-02 15:11:49 +0000

    deploy: f50ebef90c898360230ad8901ddfcefe95dfe26d

---
+++ storybook/assets/paste.stories-CJ0Se8UJ.js
@@ -0,0 +1,12 @@
+import{O as o}from"./Orchestrator-Dy5hzTCU.js";import"./___vite-browser-external_commonjs-proxy-CbhpTJ3e.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-B8XB3FuZ.js";import"./i18n-BTGr9zYS.js";import"./inheritsLoose-Cza2Y55y.js";const i={cleaning:{},components:[{id:"seq",componentType:"Sequence",label:{value:'"Sequence label"',type:"VTL|MD"},description:{value:`"je suis la description saut de ligne / 
+ Maintenant, encore un autre saut à la ligne, des explications :\r
+- tiret un\r
+- tiret deux\r
+"`,type:"VTL|MD"},conditionFilter:{value:"true",type:"VTL"},page:"1",declarations:[{declarationType:"INSTRUCTION",id:"jruq5os5-kqhuxnyt",label:{type:"VTL|MD",value:`"
+
+ declaration instruction after label!"`},position:"AFTER_QUESTION_TEXT"},{declarationType:"INSTRUCTION",id:"jruq5os5-kqhfdsfdsuxnyt",label:{type:"VTL|MD",value:'"declaration instruction before label"'},position:"BEFORE_QUESTION_TEXT"}]},{id:"seq",componentType:"Sequence",label:{value:'"Sequence label"',type:"VTL"},conditionFilter:{value:"true",type:"VTL"},page:"2"}],variables:[],pagination:"question",resizing:{},label:{type:"VTL",value:'"Example Sequence"'},lunaticModelVersion:"2.2.13",modele:"SEQUENCE",enoCoreVersion:"2.3.10",generatingDate:"05-09-2022 14:37:43",missing:!1,id:"l7ovbqou",maxPage:"2"},r={COLLECTED:{NAME:{COLLECTED:"Bob Dylan"}}},m={title:"Test/Paste",component:o,parameters:{controls:{include:["initialPage","source","data","autoSuggesterLoading"]},docs:{description:{component:"You can paste your questionnaire here to test it."}}},argTypes:{initialPage:{table:{disable:!1}},autoSuggesterLoading:{table:{disable:!1}}}},e={args:{source:i,data:r}};var t,a,n;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    source,
+    data
+  }
+} satisfies Story`,...(n=(a=e.parameters)==null?void 0:a.docs)==null?void 0:n.source}}};const g=["Default"];export{e as Default,g as __namedExportsOrder,m as default};
