062c4a82980a2fc3a31677a9091d45f3feed6c5a
Dylan Decrulle <81740200+ddecrulle@users.noreply.github.com>
2024-06-05 17:49:36 +0200

    upgrade lunatic

--- src/RadioGroup.tsx
+++ src/RadioGroup.tsx
@@ -4,9 +4,14 @@ import { RadioButtons } from "@codegouvfr/react-dsfr/RadioButtons";
 import { useId } from "react";
 
 export const RadioGroup: LunaticSlotComponents["RadioGroup"] = props => {
-    const { options, value, label, description, onSelect, errors, disabled, readOnly } = props;
+    const { options, value, label, description, errors, disabled, readOnly } = props;
 
     const id = useId();
+    /**
+     * Note that the error message ID follows the format `${id}-messages` because this is the convention used by the underlying library react-dsfr
+     * See: https://github.com/codegouvfr/react-dsfr/blob/4c41367febcb78307f261df1b761fedb52c8a905/src/shared/Fieldset.tsx#L101
+     */
+    const errorMessageId = `${id}-messages`;
 
     const { state, stateRelatedMessage } = getErrorStates(errors);
 
@@ -21,7 +26,10 @@ export const RadioGroup: LunaticSlotComponents["RadioGroup"] = props => {
                 hintText: option.description,
                 nativeInputProps: {
                     checked: value === option.value,
-                    onChange: () => onSelect(option.value),
+                    onChange: option.onCheck,
+                    ...(state === "error"
+                        ? { "aria-invalid": true, "aria-errormessage": errorMessageId }
+                        : {}),
                 },
             }))}
             state={state}
