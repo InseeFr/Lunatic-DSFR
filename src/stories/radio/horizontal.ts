7e86f128098c0167084c022216018d9afc8583b5
Dylan Decrulle <dylan.decrulle@outlook.fr>
2024-06-17 17:21:54 +0200

    add orientation feature on Radio

---
+++ src/stories/radio/horizontal.ts
@@ -0,0 +1,81 @@
+import type { LunaticSource } from "@inseefr/lunatic";
+
+export const source: LunaticSource = {
+    "cleaning": {},
+    "components": [
+        {
+            "id": "radiototo",
+            "bindingDependencies": ["Q1"],
+            "componentType": "Radio",
+            "mandatory": false,
+            "label": {
+                "value": '"Does Spongebob live in a pineapple under the sea?"',
+                "type": "VTL|MD",
+            },
+            "description": {
+                "value": '"Spongebob works for Mr. Krabs"',
+                "type": "VTL|MD",
+            },
+            "page": "1",
+            "conditionFilter": { "value": "true", "type": "VTL" },
+            "orientation": "horizontal",
+            "controls": [
+                {
+                    "id": "radio",
+                    "typeOfControl": "CONSISTENCY",
+                    "criticality": "WARN",
+                    "control": { "value": 'not(Q1= "2")', "type": "VTL" },
+                    "errorMessage": {
+                        "value": '"What else would Spongebob live in?"',
+                        "type": "VTL|MD",
+                    },
+                    "bindingDependencies": ["Q1"],
+                },
+            ],
+            "options": [
+                {
+                    "value": "1",
+                    "description": {
+                        "value": '"Spongebob lives in a pineapple"',
+                        "type": "VTL|MD",
+                    },
+                    "label": { "value": '"Yes"', "type": "VTL|MD" },
+                },
+                {
+                    "value": "2",
+                    "description": {
+                        "value": '"Spongebob does not live in a pineapple"',
+                        "type": "VTL|MD",
+                    },
+                    "label": { "value": '"No"', "type": "VTL|MD" },
+                },
+            ],
+            "response": { "name": "Q1" },
+        },
+    ],
+    "variables": [
+        {
+            "variableType": "COLLECTED",
+            "name": "Q1",
+            "values": {
+                "PREVIOUS": null,
+                "COLLECTED": null,
+                "FORCED": null,
+                "EDITED": null,
+                "INPUTTED": null,
+            },
+        },
+    ],
+    "resizing": {},
+    "label": {
+        "type": "VTL|MD",
+        "value": "Test-Dylan",
+    },
+    "lunaticModelVersion": "2.5.0",
+    "modele": "TESTDYLAN",
+    "enoCoreVersion": "2.7.1",
+    "generatingDate": "06-03-2024 12:46:44",
+    "missing": false,
+    "id": "lsvuvtbg",
+    "maxPage": "1",
+};
