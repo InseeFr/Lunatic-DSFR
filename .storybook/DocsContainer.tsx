c6bedd0314c793a61a705b08e57b249e07929c29
Dylan Decrulle <dylan.decrulle@outlook.fr>
2024-07-03 14:01:22 +0200

    fix storybook docs issue and ugrade dep

--- .storybook/DocsContainer.tsx
+++ .storybook/DocsContainer.tsx
@@ -1,26 +1,27 @@
 import { useIsDark } from "@codegouvfr/react-dsfr/useIsDark";
-import { DocsContainer as DocsContainer_base } from "@storybook/addon-docs";
+import { DocsContainer as DocsContainer_base } from "@storybook/blocks";
 import React, { ComponentProps, useEffect } from "react";
-import { useDarkMode } from "storybook-dark-mode";
 import { darkTheme, lightTheme } from "./customTheme";
+import { DARK_MODE_EVENT_NAME } from "storybook-dark-mode";
+
+import { addons } from "@storybook/preview-api";
+const channel = addons.getChannel();
 
 type Props = ComponentProps<typeof DocsContainer_base>;
 
 export function DocsContainer(props: Props) {
     const { children, context } = props;
-    const isStorybookUiDark = useDarkMode();
-    const { setIsDark } = useIsDark();
 
-    useEffect(
-        //Docs Page and storybookUI must have same theme
-        () => {
-            setIsDark(isStorybookUiDark);
-        },
-        [isStorybookUiDark],
-    );
+    const { setIsDark, isDark } = useIsDark();
+
+    useEffect(() => {
+        // listen to DARK_MODE event
+        channel.on(DARK_MODE_EVENT_NAME, setIsDark);
+        return () => channel.off(DARK_MODE_EVENT_NAME, setIsDark);
+    }, [channel, setIsDark]);
 
     return (
-        <DocsContainer_base context={context} theme={isStorybookUiDark ? darkTheme : lightTheme}>
+        <DocsContainer_base context={context} theme={isDark ? darkTheme : lightTheme}>
             {children}
         </DocsContainer_base>
     );
--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,9 +1,9 @@
 import type { StorybookConfig } from "@storybook/react-vite";
-import { mergeConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default {
     stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+
     addons: [
         "@storybook/addon-links",
         "@storybook/addon-essentials",
@@ -11,17 +11,26 @@ export default {
         "storybook-dark-mode",
         "@storybook/addon-a11y",
     ],
+
     docs: {
         //👇 See the table below for the list of supported options
         autodocs: true,
         defaultName: "Documentation",
     },
+
     framework: {
         name: "@storybook/react-vite",
         options: {},
     },
+
+    core: {
+        builder: "@storybook/builder-vite",
+    },
     staticDirs: ["./static"],
+
     async viteFinal(config) {
+        const { mergeConfig } = await import("vite");
+
         // Merge custom configuration into the default config
         return mergeConfig(config, {
             plugins: [tsconfigPaths()],
