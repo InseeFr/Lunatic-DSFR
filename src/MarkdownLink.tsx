76096dfd53344ba30927955d992bbccaf49c0b99
Dylan Decrulle <dylan.decrulle@outlook.fr>
2024-07-11 14:23:04 +0200

    improve tooltip

--- src/MarkdownLink.tsx
+++ src/MarkdownLink.tsx
@@ -1,25 +1,42 @@
+import { fr } from "@codegouvfr/react-dsfr";
 import type { LunaticSlotComponents } from "@inseefr/lunatic";
 import { useId } from "react";
+import { useStyles } from "tss-react";
 
 export const MarkdownLink: LunaticSlotComponents["MarkdownLink"] = props => {
     const { href, children, tooltip } = props;
 
-    console.log(props);
     const id = useId();
+    const { css } = useStyles();
 
     return (
         <>
-            <a
-                {...(href !== "#" ? { target: "_blank", rel: "noopener noreferrer" } : {})}
-                id={id}
-                href={href}
-                aria-describedby={tooltip ? `tooltip-${id}` : undefined}
-            >
-                {children}
-            </a>
+            {href !== "." ? (
+                <a
+                    {...(href !== "#" ? { target: "_blank", rel: "noopener noreferrer" } : {})}
+                    id={id}
+                    href={href}
+                    aria-describedby={tooltip ? `tooltip-${id}` : undefined}
+                >
+                    {children}
+                </a>
+            ) : (
+                <span
+                    id={id}
+                    aria-describedby={tooltip ? `tooltip-${id}` : undefined}
+                    className={css({
+                        textDecoration: "underline",
+                        textDecorationThickness: "var(--underline-thickness)",
+                        textUnderlineOffset: 4,
+                        ":hover": { textDecorationThickness: "calc(var(--underline-thickness) * 2)" },
+                    })}
+                >
+                    {children}
+                </span>
+            )}
             {tooltip && (
                 <span
-                    className="fr-tooltip fr-placement"
+                    className={fr.cx("fr-tooltip", "fr-placement")}
                     id={`tooltip-${id}`}
                     role="tooltip"
                     aria-hidden="true"
